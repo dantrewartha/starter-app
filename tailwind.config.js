f99c222e754dc95b87b8a0ffedeb314891bce258
Dan Trewartha <dantrewartha@gmail.com>
2024-12-26 17:24:32 -0600

    add talwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1D4ED8',
+          secondary: '#9333EA'
+        }
+      }
+    },
+    plugins: []
+  }
